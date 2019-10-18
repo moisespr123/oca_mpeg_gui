90d3449430fd0bb85964d473bc2ae7eb380d6c2e
moisesmcardona <moises@moises-studios.com>
2019-10-17 20:41:13 -0400

    Add project files.

---
+++ oca_mpeg_gui/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.StartBtn = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ChunkSizeTxt = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.CompressionLevel = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ExtractRButton = New System.Windows.Forms.RadioButton()
+        Me.CompressRButton = New System.Windows.Forms.RadioButton()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OutputLocation = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.InputLocation = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.SaveLogBtn = New System.Windows.Forms.Button()
+        Me.ClearLogBtn = New System.Windows.Forms.Button()
+        Me.Log = New System.Windows.Forms.RichTextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.CompressionLevel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.74851!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.25149!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel2, 1, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(839, 341)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.StartBtn)
+        Me.Panel1.Controls.Add(Me.GroupBox2)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.OutputLocation)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.InputLocation)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(3, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(402, 335)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 303)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(123, 26)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "v0.1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "GUI By: Moisés Cardona"
+        '
+        'StartBtn
+        '
+        Me.StartBtn.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.StartBtn.Location = New System.Drawing.Point(9, 255)
+        Me.StartBtn.Name = "StartBtn"
+        Me.StartBtn.Size = New System.Drawing.Size(390, 43)
+        Me.StartBtn.TabIndex = 15
+        Me.StartBtn.Text = "Start!"
+        Me.StartBtn.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.ChunkSizeTxt)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.CompressionLevel)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(9, 183)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(223, 66)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Compression options:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(182, 39)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "MB"
+        '
+        'ChunkSizeTxt
+        '
+        Me.ChunkSizeTxt.Location = New System.Drawing.Point(114, 33)
+        Me.ChunkSizeTxt.Name = "ChunkSizeTxt"
+        Me.ChunkSizeTxt.Size = New System.Drawing.Size(61, 20)
+        Me.ChunkSizeTxt.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(111, 16)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Chunk Size:"
+        '
+        'CompressionLevel
+        '
+        Me.CompressionLevel.Location = New System.Drawing.Point(9, 33)
+        Me.CompressionLevel.Maximum = New Decimal(New Integer() {12, 0, 0, 0})
+        Me.CompressionLevel.Name = "CompressionLevel"
+        Me.CompressionLevel.Size = New System.Drawing.Size(96, 20)
+        Me.CompressionLevel.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Compression Level:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ExtractRButton)
+        Me.GroupBox1.Controls.Add(Me.CompressRButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 9)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(157, 55)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Action:"
+        '
+        'ExtractRButton
+        '
+        Me.ExtractRButton.AutoSize = True
+        Me.ExtractRButton.Location = New System.Drawing.Point(84, 20)
+        Me.ExtractRButton.Name = "ExtractRButton"
+        Me.ExtractRButton.Size = New System.Drawing.Size(58, 17)
+        Me.ExtractRButton.TabIndex = 1
+        Me.ExtractRButton.TabStop = True
+        Me.ExtractRButton.Text = "Extract"
+        Me.ExtractRButton.UseVisualStyleBackColor = True
+        '
+        'CompressRButton
+        '
+        Me.CompressRButton.AutoSize = True
+        Me.CompressRButton.Location = New System.Drawing.Point(7, 20)
+        Me.CompressRButton.Name = "CompressRButton"
+        Me.CompressRButton.Size = New System.Drawing.Size(71, 17)
+        Me.CompressRButton.TabIndex = 0
+        Me.CompressRButton.TabStop = True
+        Me.CompressRButton.Text = "Compress"
+        Me.CompressRButton.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(324, 144)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(324, 93)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OutputLocation
+        '
+        Me.OutputLocation.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.OutputLocation.Location = New System.Drawing.Point(9, 146)
+        Me.OutputLocation.Name = "OutputLocation"
+        Me.OutputLocation.Size = New System.Drawing.Size(309, 20)
+        Me.OutputLocation.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 130)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(241, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Browse for a location to store the compressed file:"
+        '
+        'InputLocation
+        '
+        Me.InputLocation.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.InputLocation.Location = New System.Drawing.Point(9, 95)
+        Me.InputLocation.Name = "InputLocation"
+        Me.InputLocation.Size = New System.Drawing.Size(309, 20)
+        Me.InputLocation.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Browse for an input file:"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.SaveLogBtn)
+        Me.Panel2.Controls.Add(Me.ClearLogBtn)
+        Me.Panel2.Controls.Add(Me.Log)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(411, 3)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(425, 335)
+        Me.Panel2.TabIndex = 1
+        '
+        'SaveLogBtn
+        '
+        Me.SaveLogBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SaveLogBtn.Location = New System.Drawing.Point(218, 255)
+        Me.SaveLogBtn.Name = "SaveLogBtn"
+        Me.SaveLogBtn.Size = New System.Drawing.Size(204, 43)
+        Me.SaveLogBtn.TabIndex = 2
+        Me.SaveLogBtn.Text = "Save Log"
+        Me.SaveLogBtn.UseVisualStyleBackColor = True
+        '
+        'ClearLogBtn
+        '
+        Me.ClearLogBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.ClearLogBtn.Location = New System.Drawing.Point(3, 255)
+        Me.ClearLogBtn.Name = "ClearLogBtn"
+        Me.ClearLogBtn.Size = New System.Drawing.Size(204, 43)
+        Me.ClearLogBtn.TabIndex = 1
+        Me.ClearLogBtn.Text = "Clear Log"
+        Me.ClearLogBtn.UseVisualStyleBackColor = True
+        '
+        'Log
+        '
+        Me.Log.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Log.Location = New System.Drawing.Point(3, 3)
+        Me.Log.Name = "Log"
+        Me.Log.Size = New System.Drawing.Size(419, 246)
+        Me.Log.TabIndex = 0
+        Me.Log.Text = ""
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(839, 341)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "oca_mpeg GUI"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.CompressionLevel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ExtractRButton As RadioButton
+    Friend WithEvents CompressRButton As RadioButton
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OutputLocation As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents InputLocation As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents StartBtn As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ChunkSizeTxt As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents CompressionLevel As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Log As RichTextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents SaveLogBtn As Button
+    Friend WithEvents ClearLogBtn As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ oca_mpeg_gui/Form1.vb
@@ -0,0 +1,145 @@
+﻿Public Class Form1
+
+    Private Sub CheckAndAdjust()
+        If IO.Path.GetExtension(InputLocation.Text).Contains(".oca") Then ExtractRButton.Checked = True Else CompressRButton.Checked = True
+        If ExtractRButton.Checked Then
+            OutputLocation.Text = IO.Path.GetFileNameWithoutExtension(InputLocation.Text)
+        Else
+            OutputLocation.Text = InputLocation.Text + ".oca"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Filter = "All Files|*.*"
+        If CompressRButton.Checked Then
+            OpenFileDialog1.Title = "Browse for a file to compress"
+        Else
+            OpenFileDialog1.Title = "Browse for a file to extract"
+        End If
+        If Not String.IsNullOrWhiteSpace(InputLocation.Text) Then OpenFileDialog1.FileName = IO.Path.GetFileName(InputLocation.Text)
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog
+        If result = DialogResult.OK Then
+            InputLocation.Text = OpenFileDialog1.FileName
+            CheckAndAdjust()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If CompressRButton.Checked Then
+            SaveFileDialog1.Filter = "OCA File|*.oca"
+            SaveFileDialog1.Title = "Browse for a location to save the compressed OCA file"
+        Else
+            SaveFileDialog1.Filter = "All Files|*.*"
+            SaveFileDialog1.Title = "Browse for a location to save the extracted file"
+        End If
+        If Not String.IsNullOrWhiteSpace(OutputLocation.Text) Then SaveFileDialog1.FileName = IO.Path.GetFileName(OutputLocation.Text)
+        Dim result As DialogResult = SaveFileDialog1.ShowDialog
+        If result = DialogResult.OK Then OutputLocation.Text = SaveFileDialog1.FileName
+
+    End Sub
+
+    Private Sub StartBtn_Click(sender As Object, e As EventArgs) Handles StartBtn.Click
+        StartBtn.Enabled = False
+        SaveLogBtn.Enabled = False
+        Dim CMD As String = String.Empty
+        If CompressRButton.Checked Then
+            CMD = CompressionLevel.Value.ToString() + " " + ChunkSizeTxt.Text + " """ + OutputLocation.Text + """ """ + InputLocation.Text + """"
+        Else
+            CMD = "d """ + OutputLocation.Text + """ """ + InputLocation.Text + """"
+        End If
+        Dim StartThread = New Threading.Thread(Sub() TaskThread(CMD))
+        StartThread.Start()
+    End Sub
+
+    Private Sub TaskThread(CMD As String)
+        Using process As New Process()
+            process.StartInfo.WorkingDirectory = IO.Path.GetDirectoryName("oca_mpeg.exe")
+            process.StartInfo.FileName = "oca_mpeg.exe"
+            process.StartInfo.Arguments = CMD
+            process.StartInfo.UseShellExecute = False
+            process.StartInfo.RedirectStandardOutput = True
+            process.StartInfo.RedirectStandardError = True
+            process.StartInfo.CreateNoWindow = True
+            process.Start()
+            AddHandler process.OutputDataReceived, New DataReceivedEventHandler(AddressOf UpdateLogEventHandler)
+            AddHandler process.ErrorDataReceived, New DataReceivedEventHandler(AddressOf UpdateLogEventHandler)
+            process.BeginOutputReadLine()
+            process.BeginErrorReadLine()
+            process.WaitForExit()
+            StartBtn.BeginInvoke(Sub() StartBtn.Enabled = True)
+            SaveLogBtn.BeginInvoke(Sub() SaveLogBtn.Enabled = True)
+        End Using
+    End Sub
+
+    Private Sub UpdateLogEventHandler(sender As Object, e As DataReceivedEventArgs)
+        If Not e.Data = Nothing Then
+            UpdateLog(e.Data)
+        End If
+    End Sub
+
+    Private Delegate Sub UpdateLogInvoker(message As String)
+
+    Private Sub UpdateLog(message As String)
+        If Log.InvokeRequired Then
+            Log.Invoke(New UpdateLogInvoker(AddressOf UpdateLog), message)
+        Else
+            If Not String.IsNullOrWhiteSpace(message) Then
+                Log.AppendText(Date.Now().ToString() + " || " + message + vbCrLf)
+                Log.SelectionStart = Log.Text.Length - 1
+                Log.ScrollToCaret()
+            End If
+        End If
+    End Sub
+
+    Private Sub CompressRButton_CheckedChanged(sender As Object, e As EventArgs) Handles CompressRButton.CheckedChanged
+        My.Settings.CompressChecked = CompressRButton.Checked
+        My.Settings.Save()
+    End Sub
+
+    Private Sub ExtractRButton_CheckedChanged(sender As Object, e As EventArgs) Handles ExtractRButton.CheckedChanged
+        My.Settings.ExtractChecked = ExtractRButton.Checked
+        My.Settings.Save()
+    End Sub
+
+    Private Sub CompressionLevel_ValueChanged(sender As Object, e As EventArgs) Handles CompressionLevel.ValueChanged
+        My.Settings.CompressionLevel = CompressionLevel.Value
+        My.Settings.Save()
+    End Sub
+
+    Private Sub ChunkSizeTxt_TextChanged(sender As Object, e As EventArgs) Handles ChunkSizeTxt.TextChanged
+        My.Settings.ChunkSize = ChunkSizeTxt.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CompressRButton.Checked = My.Settings.CompressChecked
+        ExtractRButton.Checked = My.Settings.ExtractChecked
+        CompressionLevel.Value = My.Settings.CompressionLevel
+        ChunkSizeTxt.Text = My.Settings.ChunkSize
+        CompressionLevel.Minimum = 1
+    End Sub
+
+    Private Sub ClearLogButton_Click(sender As Object, e As EventArgs) Handles ClearLogBtn.Click
+        Log.Text = String.Empty
+    End Sub
+
+    Private Sub SaveLogButton_Click(sender As Object, e As EventArgs) Handles SaveLogBtn.Click
+        Dim SaveDialog As New SaveFileDialog With {
+           .Filter = "Text file|*.txt",
+           .FileName = String.Empty,
+           .Title = "Browse to save the log"}
+        Dim result As DialogResult = SaveDialog.ShowDialog
+        If result = DialogResult.OK Then
+            If Not String.IsNullOrWhiteSpace(SaveDialog.FileName) Then IO.File.WriteAllText(SaveDialog.FileName, Log.Text)
+        End If
+    End Sub
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        InputLocation.Text = CType(e.Data.GetData(DataFormats.FileDrop), String())(0)
+        CheckAndAdjust()
+    End Sub
+End Class
